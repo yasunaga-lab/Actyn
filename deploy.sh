bdf33fe7bd54357a20a41f12c6c75afd1d89fdcc
Yasuhisa HONDA <nqvno14@gmail.com>
2022-12-26 11:28:06 +0900

    【ansible】remove nginx operation, modified execution

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ ng build --base-href=/ --configuration=production
 # copy client to webserver
 cd ../
 rm -rf yacm_ws/src/main/resources/static/*
-mv yacm_cl/dist/yacm_cl/* yacm_ws/src/main/resources/static/
+cp -R yacm_cl/dist/yacm_cl/* yacm_ws/src/main/resources/static/
 
 # build actyn web server
 cd yacm_ws/
@@ -20,4 +20,6 @@ cd yacm_ws_ldap/
 cd ../
 
 # deployment by ansible toward each server.
-ansible-playbook -i ansible/inventory/actyn_hosts -k ansible/actyn-playbook.yml
+echo ""
+echo "### Password for deployment by ansible is \"easy password (from k)\". ###"
+ansible-playbook -i ansible/inventory/actyn_hosts.yml -e @ansible/inventory/actyn_vault.yml ansible/actyn-playbook.yml --ask-vault-pass
